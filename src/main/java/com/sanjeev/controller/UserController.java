e29c66ff1d9706873bb1b680c0aa6acfadbc5958
sanjeev <sanjeev.jha@intelligrape.com>
2016-08-18 03:35:52 +0530

    added hibernate configuration

--- src/main/java/com/sanjeev/controller/UserController.java
+++ src/main/java/com/sanjeev/controller/UserController.java
@@ -1,14 +1,23 @@
 package com.sanjeev.controller;
 
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+import org.springframework.beans.factory.BeanFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.xml.XmlBeanFactory;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.FileSystemResource;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.context.WebApplicationContext;
 
+import com.sanjeev.domain.User;
 import com.sanjeev.service.Address;
 import com.sanjeev.service.UserDetail;
 import com.sanjeev.service.UserService;
@@ -27,6 +36,10 @@ public class UserController {
 	@ResponseBody
 	public String hello(){
 		 
+		//TODO basic way to load config file
+		BeanFactory beanFactory1=new XmlBeanFactory(new ClassPathResource("config.xml"));
+		BeanFactory beanFactory2=new XmlBeanFactory(new FileSystemResource("/home/projects/java/_eclipse/src/main/resource/config.xml"));
+		UserService uservice=(UserService)beanFactory1.getBean("userService");
 		 
 		 UserService service=context.getBean("userService",UserService.class);
 		 System.out.println("###########userService############");
@@ -49,5 +62,17 @@ public class UserController {
 		 System.out.println(userService.toString());
 		return "request";
 	}*/
+	
+	@RequestMapping("/test/hibernate")
+	@ResponseBody
+	public String hibernateDemo(){
+		org.hibernate.cfg.Configuration configuration=new Configuration().configure("hibernate.cfg.xml");
+		SessionFactory factory=configuration.buildSessionFactory();
+		Session session=factory.openSession();
+		Transaction transaction=session.beginTransaction();
+		session.save(new User());
+		transaction.commit();
+		return "success";
+	}
 
 }
