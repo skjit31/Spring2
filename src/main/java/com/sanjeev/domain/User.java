bcc52665d0cd432b48f8fd9637eb28ff458db00f
sanjeev <sanjeev.jha@intelligrape.com>
2016-08-18 04:25:32 +0530

    added connection pool configuration

---
+++ src/main/java/com/sanjeev/domain/User.java
@@ -0,0 +1,55 @@
+package com.sanjeev.domain;
+
+import java.io.Serializable;
+
+public class User implements Serializable{
+	
+	private int id;
+	private String username;
+	private String password;
+	private String email;
+
+	public User() {
+		this.id=1;
+		this.username="sanjeev";
+		this.password="password";
+		this.email="sanjeev@gmail.com";
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+	
+	
+	
+	
+
+}
